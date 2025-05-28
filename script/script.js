c7dca40f024c7b51f76bc85fb6b629c70e5037c8
Anar_Osmanov <anar_osmanov@vk.com>
2025-05-29 03:46:58 +0400

    Добавление кнопок скролла для девайсов на главной странице

--- script/script.js
+++ script/script.js
@@ -1,17 +1,28 @@
-let burgerEl = document.querySelector('#burgerMenuFormediaScreen')
-let navEl = document.querySelector('.nav ul')
+//Элементы бургер кнопки
+let burgerEl = document.querySelector("#burgerMenuFormediaScreen");
+let navEl = document.querySelector(".nav ul");
 
+//Элементы для скролла девайсов
+let nowrapControllersEl = document.querySelector(".nowrap");
+let prevBtn = document.querySelector("#prev");
+let nextBtn = document.querySelector("#next");
 
-burgerEl.onclick = () => {
-    showNavigation()
-
-    function showNavigation() {
-
-        if (navEl.style.display === 'flex') {
-            navEl.style.display = 'none'
-        } else {
-            navEl.style.display = 'flex'
-        }
+burgerEl.addEventListener("click", function showNavigation() {
+    if (navEl.style.display === "flex") {
+        navEl.style.display = "none";
+    } else {
+        navEl.style.display = "flex";
     }
+});
 
-}
+prevBtn.addEventListener("click", () => {
+    nowrapControllersEl.scrollBy({
+        left: -150,
+    });
+});
+
+nextBtn.addEventListener("click", () => {
+    nowrapControllersEl.scrollBy({
+        left: 100
+    });
+});
